a9f94c1747ec22be223e1f8c728848d53f13a63d
Maxime Chambreuil <maxime.chambreuil@savoirfairelinux.com>
2015-02-07 17:19:03 -0500

    [IMP] Add help

--- module_prototyper/models/module_prototyper.py
+++ module_prototyper/models/module_prototyper.py
@@ -57,17 +57,29 @@ class ModulePrototyper(models.Model):
         help=('The Module Name will be used as the displayed name of the '
               'exported module.')
     )
-    summary = fields.Char('Summary', required=True)
-    description = fields.Text('Description', required=True)
-    author = fields.Char('Author', required=True)
-    maintainer = fields.Char('Maintainer')
-    website = fields.Char('Website')
+    summary = fields.Char('Summary', required=True, 
+        help=('Enter a summary of your module')
+    )
+    description = fields.Text('Description', required=True, 
+        help=('Enter the description of your module, what it does, how to'
+              'install, configure and use it, the roadmap or known issues')
+    )
+    author = fields.Char('Author', required=True,
+        help=('Enter your name')
+    )
+    maintainer = fields.Char('Maintainer', 
+        help=('Enter the name of the person or organization who will'
+              'maintain this module')
+    )
+    website = fields.Char('Website', help=('Enter the URL of your website'))
     icon_image = fields.Binary(
         'Icon',
         help=('The icon set up here will be used as the icon '
               'for the exported module also')
     )
-    version = fields.Char('Version', size=3, default='0.1')
+    version = fields.Char('Version', size=3, default='0.1',
+        help=('Enter the version of your module with 2 digits')
+    )
     auto_install = fields.Boolean(
         'Auto Install',
         default=False,
@@ -82,7 +94,9 @@ class ModulePrototyper(models.Model):
     dependency_ids = fields.Many2many(
         'ir.module.module', 'module_prototyper_module_rel',
         'module_prototyper_id', 'module_id',
-        'Dependencies'
+        'Dependencies',
+        help=('Enter the list of required modules that need to be installed'
+              'for your module to work properly')
     )
     data_ids = fields.Many2many(
         'ir.filters',
@@ -100,28 +114,42 @@ class ModulePrototyper(models.Model):
     )
     field_ids = fields.Many2many(
         'ir.model.fields', 'prototype_fields_rel',
-        'module_prototyper_id', 'field_id', 'Fields'
+        'module_prototyper_id', 'field_id', 'Fields',
+        help=('Enter the list of fields that you have created or modified'
+              'and want to export in this module. New models will be'
+              'exported as long as you choose one of his fields.')
     )
     menu_ids = fields.Many2many(
         'ir.ui.menu', 'prototype_menu_rel',
-        'module_prototyper_id', 'menu_id', 'Menu Items'
+        'module_prototyper_id', 'menu_id', 'Menu Items',
+        help=('Enter the list of menu items that you have created and want'
+              'to export in this module. Related windows actions will be'
+              'exported as well.')
     )
     view_ids = fields.Many2many(
         'ir.ui.view', 'prototype_view_rel',
-        'module_prototyper_id', 'view_id', 'Views'
+        'module_prototyper_id', 'view_id', 'Views',
+        help=('Enter the list of views that you have created and want to'
+              'export in this module.')
     )
     group_ids = fields.Many2many(
         'res.groups', 'prototype_groups_rel',
-        'module_prototyper_id', 'group_id', 'Groups'
+        'module_prototyper_id', 'group_id', 'Groups',
+        help=('Enter the list of groups that you have created and want to'
+              'export in this module.')
     )
     right_ids = fields.Many2many(
         'ir.model.access', 'prototype_rights_rel',
         'module_prototyper_id', 'right_id',
-        'Access Rights'
+        'Access Rights',
+        help=('Enter the list of access rights that you have created and'
+              'want to export in this module.')
     )
     rule_ids = fields.Many2many(
         'ir.rule', 'prototype_rule_rel',
-        'module_prototyper_id', 'rule_id', 'Record Rules'
+        'module_prototyper_id', 'rule_id', 'Record Rules',
+        help=('Enter the list of record rules that you have created and'
+              'want to export in this module.')
     )
 
     __data_files = []
